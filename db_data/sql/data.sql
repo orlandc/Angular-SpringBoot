48d493e692ef643c83f918d6341443d4fad01a47
orlandc <orlando.montenegro.r@gmail.com>
2021-10-12 07:28:41 -0500

    modify schema db and firts insert data

--- db_data/sql/data.sql
+++ db_data/sql/data.sql
@@ -28,6 +28,15 @@ INSERT INTO "subarea" VALUES(40, 'Logistica', 4);
 INSERT INTO "subarea" VALUES(41, 'Produccion', 4);
 INSERT INTO "subarea" VALUES(42, 'Calidad', 4);
 
+/*
+** ----------------------------------------------------------------------------
+** script to insert data into typenui table
+** ----------------------------------------------------------------------------
+*/
+INSERT INTO "typenui" ("id_typenui", "typenui") VALUES (1, 'Cedula');
+INSERT INTO "typenui" ("id_typenui", "typenui") VALUES (2, 'T. Identidad');
+INSERT INTO "typenui" ("id_typenui", "typenui") VALUES (3, 'Pasaporte');
+INSERT INTO "typenui" ("id_typenui", "typenui") VALUES (4, 'Cedula de Extranjeria');
 
 /*
 ** ----------------------------------------------------------------------------
@@ -42,60 +51,60 @@ INSERT INTO "rol" VALUES(2, 'ROLE_USER');
 ** script to insert data into employees table 
 ** ----------------------------------------------------------------------------
 */
-INSERT INTO "employees" VALUES (198, 1, 'Donald', 'OConnell', 1, 10);
-INSERT INTO "employees" VALUES (199, 1, 'Douglas', 'Grant', 1, 10);
-INSERT INTO "employees" VALUES (200, 1, 'Jennifer', 'Whalen', 1, 11);
-INSERT INTO "employees" VALUES (201, 1, 'Michael', 'Hartstein', 1, 11);
-INSERT INTO "employees" VALUES (202, 1, 'Pat', 'Fay', 1, 12);
-INSERT INTO "employees" VALUES (203, 1, 'Susan', 'Mavris', 1, 12);
-INSERT INTO "employees" VALUES (204, 1, 'Hermann', 'Baer', 2, 20);
-INSERT INTO "employees" VALUES (205, 1, 'Shelley', 'Higgins', 2, 20);
-INSERT INTO "employees" VALUES (206, 1, 'William', 'Gietz', 2, 21);
-INSERT INTO "employees" VALUES (100, 1, 'Steven', 'King', 2, 21);
-INSERT INTO "employees" VALUES (101, 1, 'Neena', 'Kochhar', 2, 22);
-INSERT INTO "employees" VALUES (102, 2, 'Lex', 'De Haan', 2, 22);
-INSERT INTO "employees" VALUES (103, 2, 'Alexander', 'Hunold', 2, 23);
-INSERT INTO "employees" VALUES (104, 2, 'Bruce', 'Ernest', 2, 23);
-INSERT INTO "employees" VALUES (105, 2, 'David', 'Austin', 3, 30);
-INSERT INTO "employees" VALUES (106, 2, 'Valli', 'Pataballa', 3, 30);
-INSERT INTO "employees" VALUES (107, 2, 'Diana', 'Lorentz', 3, 31);
-INSERT INTO "employees" VALUES (108, 2, 'Nancy', 'Greenberg', 3, 31);
-INSERT INTO "employees" VALUES (109, 2, 'Daniel', 'Faviet', 3, 32);
-INSERT INTO "employees" VALUES (110, 2, 'John', 'Chen', 3, 32);
-INSERT INTO "employees" VALUES (111, 2, 'Ismael', 'Sciarra', 4, 40);
-INSERT INTO "employees" VALUES (112, 2, 'Jose Manuel', 'Urman', 4, 40);
-INSERT INTO "employees" VALUES (113, 3, 'Luis', 'Popp', 4, 41);
-INSERT INTO "employees" VALUES (114, 3, 'Den', 'Raphaely', 4, 41);
-INSERT INTO "employees" VALUES (115, 3, 'Alexander', 'Khoo', 4, 42);
-INSERT INTO "employees" VALUES (116, 3, 'Shelli', 'Baida', 4, 42);
-INSERT INTO "employees" VALUES (117, 3, 'Sigal', 'Tobias', 1, 10);
-INSERT INTO "employees" VALUES (118, 3, 'Guy', 'Himuro', 1, 10);
-INSERT INTO "employees" VALUES (119, 3, 'Karen', 'Colmenares', 1, 11);
-INSERT INTO "employees" VALUES (120, 3, 'Matthew', 'Weiss', 1, 11);
-INSERT INTO "employees" VALUES (121, 3, 'Adam', 'Fripp', 1, 12);
-INSERT INTO "employees" VALUES (122, 1, 'Payam', 'Kaufling', 1, 12);
-INSERT INTO "employees" VALUES (123, 1, 'Shanta', 'Vollman', 2, 20);
-INSERT INTO "employees" VALUES (124, 1, 'Kevin', 'Mourgos', 2, 20);
-INSERT INTO "employees" VALUES (125, 1, 'Julia', 'Nayer', 2, 21);
-INSERT INTO "employees" VALUES (126, 1, 'Irene', 'Mikkilineni', 2, 21);
-INSERT INTO "employees" VALUES (127, 1, 'James', 'Landry', 2, 22);
-INSERT INTO "employees" VALUES (128, 1, 'Steven', 'Markle', 2, 22);
-INSERT INTO "employees" VALUES (129, 1, 'Laura', 'Bissot', 2, 23);
-INSERT INTO "employees" VALUES (130, 1, 'Mozhe', 'Atkinson', 2, 23);
-INSERT INTO "employees" VALUES (131, 1, 'James', 'Marlow', 3, 30);
-INSERT INTO "employees" VALUES (132, 1, 'TJ', 'Olson', 3, 30);
-INSERT INTO "employees" VALUES (133, 2, 'Jason', 'Mallin', 3, 31);
-INSERT INTO "employees" VALUES (134, 2, 'Michael', 'Rogers', 3, 31);
-INSERT INTO "employees" VALUES (135, 2, 'Ki', 'Gee', 3, 32);
-INSERT INTO "employees" VALUES (136, 2, 'Hazel', 'Philtanker', 3, 32);
-INSERT INTO "employees" VALUES (137, 2, 'Renske', 'Ladwig', 4, 40);
-INSERT INTO "employees" VALUES (138, 2, 'Stephen', 'Stiles', 4, 40);
-INSERT INTO "employees" VALUES (139, 2, 'John', 'Seo', 4, 41);
-INSERT INTO "employees" VALUES (140, 2, 'Joshua', 'Patel', 4, 41);
+INSERT INTO "employees" VALUES (198, 1, 'Donald', 'OConnell', 10);
+INSERT INTO "employees" VALUES (199, 1, 'Douglas', 'Grant', 10);
+INSERT INTO "employees" VALUES (200, 1, 'Jennifer', 'Whalen', 11);
+INSERT INTO "employees" VALUES (201, 1, 'Michael', 'Hartstein', 11);
+INSERT INTO "employees" VALUES (202, 1, 'Pat', 'Fay', 12);
+INSERT INTO "employees" VALUES (203, 1, 'Susan', 'Mavris', 12);
+INSERT INTO "employees" VALUES (204, 1, 'Hermann', 'Baer', 20);
+INSERT INTO "employees" VALUES (205, 1, 'Shelley', 'Higgins', 20);
+INSERT INTO "employees" VALUES (206, 1, 'William', 'Gietz', 21);
+INSERT INTO "employees" VALUES (100, 1, 'Steven', 'King', 21);
+INSERT INTO "employees" VALUES (101, 1, 'Neena', 'Kochhar', 22);
+INSERT INTO "employees" VALUES (102, 2, 'Lex', 'De Haan', 22);
+INSERT INTO "employees" VALUES (103, 2, 'Alexander', 'Hunold', 23);
+INSERT INTO "employees" VALUES (104, 2, 'Bruce', 'Ernest', 23);
+INSERT INTO "employees" VALUES (105, 2, 'David', 'Austin', 30);
+INSERT INTO "employees" VALUES (106, 2, 'Valli', 'Pataballa', 30);
+INSERT INTO "employees" VALUES (107, 2, 'Diana', 'Lorentz', 31);
+INSERT INTO "employees" VALUES (108, 2, 'Nancy', 'Greenberg', 31);
+INSERT INTO "employees" VALUES (109, 2, 'Daniel', 'Faviet', 32);
+INSERT INTO "employees" VALUES (110, 2, 'John', 'Chen', 32);
+INSERT INTO "employees" VALUES (111, 2, 'Ismael', 'Sciarra', 40);
+INSERT INTO "employees" VALUES (112, 2, 'Jose Manuel', 'Urman', 40);
+INSERT INTO "employees" VALUES (113, 3, 'Luis', 'Popp', 41);
+INSERT INTO "employees" VALUES (114, 3, 'Den', 'Raphaely', 41);
+INSERT INTO "employees" VALUES (115, 3, 'Alexander', 'Khoo', 42);
+INSERT INTO "employees" VALUES (116, 3, 'Shelli', 'Baida', 42);
+INSERT INTO "employees" VALUES (117, 3, 'Sigal', 'Tobias', 10);
+INSERT INTO "employees" VALUES (118, 3, 'Guy', 'Himuro', 10);
+INSERT INTO "employees" VALUES (119, 3, 'Karen', 'Colmenares', 11);
+INSERT INTO "employees" VALUES (120, 3, 'Matthew', 'Weiss', 11);
+INSERT INTO "employees" VALUES (121, 3, 'Adam', 'Fripp', 12);
+INSERT INTO "employees" VALUES (122, 1, 'Payam', 'Kaufling', 12);
+INSERT INTO "employees" VALUES (123, 1, 'Shanta', 'Vollman', 20);
+INSERT INTO "employees" VALUES (124, 1, 'Kevin', 'Mourgos', 20);
+INSERT INTO "employees" VALUES (125, 1, 'Julia', 'Nayer', 21);
+INSERT INTO "employees" VALUES (126, 1, 'Irene', 'Mikkilineni', 21);
+INSERT INTO "employees" VALUES (127, 1, 'James', 'Landry', 22);
+INSERT INTO "employees" VALUES (128, 1, 'Steven', 'Markle', 22);
+INSERT INTO "employees" VALUES (129, 1, 'Laura', 'Bissot', 23);
+INSERT INTO "employees" VALUES (130, 1, 'Mozhe', 'Atkinson', 23);
+INSERT INTO "employees" VALUES (131, 1, 'James', 'Marlow', 30);
+INSERT INTO "employees" VALUES (132, 1, 'TJ', 'Olson', 30);
+INSERT INTO "employees" VALUES (133, 2, 'Jason', 'Mallin', 31);
+INSERT INTO "employees" VALUES (134, 2, 'Michael', 'Rogers', 31);
+INSERT INTO "employees" VALUES (135, 2, 'Ki', 'Gee', 32);
+INSERT INTO "employees" VALUES (136, 2, 'Hazel', 'Philtanker', 32);
+INSERT INTO "employees" VALUES (137, 2, 'Renske', 'Ladwig', 40);
+INSERT INTO "employees" VALUES (138, 2, 'Stephen', 'Stiles', 40);
+INSERT INTO "employees" VALUES (139, 2, 'John', 'Seo', 41);
+INSERT INTO "employees" VALUES (140, 2, 'Joshua', 'Patel', 41);
 
 /*
 ** ----------------------------------------------------------------------------
-** script to insert data into usuatio and rol table 
+** script to insert data into usuario and rol table 
 ** ----------------------------------------------------------------------------
 */
 
@@ -104,6 +113,7 @@ INSERT INTO "public"."usuario" ("id", "username", "password") VALUES (2, 'user',
 INSERT INTO "public"."usuario_rol" ("usuario_id", "rol_id") VALUES (1, 1);
 INSERT INTO "public"."usuario_rol" ("usuario_id", "rol_id") VALUES (1, 2);
 INSERT INTO "public"."usuario_rol" ("usuario_id", "rol_id") VALUES (2, 2);
+
 
 /*
 ** ----------------------------------------------------------------------------
--- db_data/sql/schema.sql
+++ db_data/sql/schema.sql
@@ -1,16 +1,23 @@
 CREATE TABLE "public"."area" (
-  "id"        int4        NOT NULL,
+  "id_area"   int4        NOT NULL,
   "nombre"    varchar(15) NOT NULL,
-  PRIMARY KEY ("id"),
-  UNIQUE ("id")
+  CONSTRAINT pk_area_id PRIMARY KEY ("id_area"),
+  UNIQUE ("id_area")
 );
 
 CREATE TABLE "public"."subarea" (
-  "id"        int4        NOT NULL,
-  "nombre"    varchar(15) NOT NULL,
-  "deparment" int4        NOT NULL,
-  PRIMARY KEY ("id"),
-  FOREIGN KEY ("deparment") REFERENCES "public"."area" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+  "id_subarea" int4        NOT NULL,
+  "nombre"     varchar(15) NOT NULL,
+  "area"       int4        NOT NULL,
+  CONSTRAINT pk_subarea_id PRIMARY KEY ("id_subarea"),
+  FOREIGN KEY ("area") REFERENCES "public"."area" ("id_area") ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+CREATE TABLE "public"."typenui" (
+  "id_typenui" int4         NOT NULL,
+  "typenui"    varchar(55)  NOT NULL,
+  CONSTRAINT pk_typenui_id PRIMARY KEY ("id_typenui"),
+  UNIQUE ("typenui")
 );
 
 CREATE TABLE "public"."employees" (
@@ -18,11 +25,10 @@ CREATE TABLE "public"."employees" (
   "typenui"   int4        NOT NULL,
   "nombres"   varchar(15) NOT NULL,
   "apellidos" varchar(15) NOT NULL,
-  "area"      int4,
   "subarea"   int4,
-  PRIMARY KEY ("nui"),
-  CONSTRAINT fk_area    FOREIGN KEY ("area")    REFERENCES "public"."area"    ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT fk_subarea FOREIGN KEY ("subarea") REFERENCES "public"."subarea" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT pk_employee_nui PRIMARY KEY ("nui"),
+  CONSTRAINT fk_typenui FOREIGN KEY ("typenui") REFERENCES "public"."typenui" ("id_typenui") ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_subarea FOREIGN KEY ("subarea") REFERENCES "public"."subarea" ("id_subarea") ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE SEQUENCE "user_id_seq";
@@ -32,20 +38,20 @@ CREATE TABLE "public"."usuario" (
     "username" varchar(15) NOT NULL,
     "password" varchar(255) NOT NULL,
     UNIQUE ("username"),
-    PRIMARY KEY ("id")
+    CONSTRAINT pk_usuario_id PRIMARY KEY ("id")
 );
 
 CREATE TABLE "public"."rol" (
     "id"         int4        NOT NULL,
     "rol_nombre" varchar(15) NOT NULL,
     UNIQUE ("rol_nombre"),
-    PRIMARY KEY ("id")
+    CONSTRAINT pk_rol_id PRIMARY KEY ("id")
 );
 
 CREATE TABLE "public"."usuario_rol" (
     "usuario_id" int4  NOT NULL,
     "rol_id"     int4  NOT NULL,
-    PRIMARY KEY ("usuario_id", "rol_id"),
+    CONSTRAINT pk_usuario_rol_id PRIMARY KEY ("usuario_id", "rol_id"),
     CONSTRAINT fk_usuario FOREIGN KEY("usuario_id") REFERENCES "public"."usuario" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT fk_rol     FOREIGN KEY("rol_id")     REFERENCES "public"."rol" ("id")     ON DELETE NO ACTION ON UPDATE NO ACTION
 );
